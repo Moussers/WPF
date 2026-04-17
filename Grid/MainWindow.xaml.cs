60d27eaf2498e8ecac8162afdacd5411dbacb793
Sand <sand999.com>
2026-04-17 21:29:49 +0300

    Grids

--- Grid/MainWindow.xaml.cs
+++ Grid/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF
+namespace Grid
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,12 +23,6 @@ namespace WPF
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //btnStart.Content = btnStart.Content == "Start" ? "Stop" : "Start";
-            (sender as Button).Content = (sender as Button).Content.ToString() == "Start" ? "Stop" : "Start";
         }
     }
 }
